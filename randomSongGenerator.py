3fc481638b22ed4eae9676eebea3e2071a3e5d1c
ChenxuMurong <99457790+ChenxuMurong@users.noreply.github.com>
2022-06-22 15:11:42 -0400

    key fixed

--- randomSongGenerator.py
+++ randomSongGenerator.py
@@ -3,8 +3,15 @@ import random as r
 
 '''returns a note of random pitch and duration
     duration accurate to a sixteenth note'''
-def random_note(MIDI_lowerbound, MIDI_upperbound, quarterlength_l, quarterlength_u):
+def random_note(MIDI_lowerbound, MIDI_upperbound, quarterlength_l, quarterlength_u, key):
     midi = r.randint(MIDI_lowerbound, MIDI_upperbound)
+    pitches = [i.midi%12 for i in key.pitches]
+
+    ''' make sure the note is in the right key'''
+
+    while midi%12 not in pitches:
+        midi = r.randint(MIDI_lowerbound, MIDI_upperbound)
+
 
     '''python random funcs can't take floats so we got to scale up the user input'''
     ql_lo_amplified = quarterlength_l * 1024
@@ -15,7 +22,7 @@ def random_note(MIDI_lowerbound, MIDI_upperbound, quarterlength_l, quarterlength
     quarterlength = r.randrange(ql_lo_amplified, ql_up_amplified + sixteenth, sixteenth)
     quarterlength /= 1024
     res = note.Note(midi, quarterLength=quarterlength)
-    res.storedInstrument = instrument.Viola()
+
     return res
 
 
@@ -39,21 +46,19 @@ if __name__ == "__main__":
     score = stream.Score()
     song = stream.Part()
 
-    song.append(meter.TimeSignature('6/8'))
-    song.append(instrument.Viola())
+    kee = key.Key('E', 'major')
+
+    song.append(meter.TimeSignature('4/4'))
+    song.append(instrument.Piano())
     song.append(tempo.MetronomeMark(text=None, number=60, referent=note.Note(type='half')))
-    song.keySignature = key.Key('C', 'major')
+    song.keySignature = kee
 
 
     for i in range(40):
-        if r.random() < 0.8:
-            song.append(random_note(55, 80, 0.25, 2))
-        elif song[-1] is not note.Rest:
-            song.append(random_rest(0.25, 0.5))
+        if r.random() < 0.8 or song[-1] is note.Rest:
+            song.append(random_note(55, 80, 0.25, 2, song.keySignature))
         else:
-            song.append(random_note(55, 80, 0.25, 2))
-
-
+            song.append(random_rest(0.25, 0.5))
 
     # bigSong = stream.Stream()
     # b = corpus.parse('bach/bwv66.6')
